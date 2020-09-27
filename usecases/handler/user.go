1f2ac0a001704f69cb7bdf99bf5ca43a3f01eebd
Hosshii <sao_heath6147.wistre@icloud.com>
2020-09-27 23:40:10 +0900

    handler

--- usecases/handler/user.go
+++ usecases/handler/user.go
@@ -6,14 +6,22 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/traPtitech/traPortfolio/domain"
 	"github.com/traPtitech/traPortfolio/usecases/repository"
+	service "github.com/traPtitech/traPortfolio/usecases/service/user_service"
 )
 
 type UserHandler struct {
 	UserRepository repository.UserRepository
+	UserService    service.UserService
 }
 
 func NewUserHandler(repo repository.UserRepository) *UserHandler {
 	return &UserHandler{UserRepository: repo}
+}
+
+func (handler *UserHandler) Get(c echo.Context) error {
+	// todo
+	// handler.UserService.GetUser()
+	return nil // todo
 }
 
 func (handler *UserHandler) Update(c echo.Context) error {
