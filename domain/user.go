8f3fc927b12945cdb29bd687582f45e329c93239
Hosshii <sao_heath6147.wistre@icloud.com>
2020-09-27 23:40:26 +0900

    model.User

--- domain/user.go
+++ domain/user.go
@@ -1,7 +1,12 @@
 package domain
 
+import "time"
+
 type User struct {
-	ID          uint
-	Description string
-	Check       bool
+	ID          uint      `gorm:"type:char(36);not null;primary_key"`
+	Description string    `sql:"type:TEXT COLLATE utf8mb4_bin NOT NULL"`
+	Check       bool      `gorm:"type:boolean;not null;default:false"`
+	Name        string    `gorm:"type:varchar(32);not null;unique"`
+	CreatedAt   time.Time `gorm:"precision:6"`
+	UpdatedAt   time.Time `gorm:"precision:6"`
 }
