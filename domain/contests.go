d3ba8a2f8c6e563e464d4947e2b41ac10e78006f
Hosshii <sao_heath6147.wistre@icloud.com>
2021-02-21 14:51:18 +0900

    add domain

---
+++ domain/contests.go
@@ -0,0 +1,29 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
+type Contest struct {
+	ID          uuid.UUID `gorm:"type:char(36);not null;primary_key"`
+	Name        string    `gorm:"type:varchar(32);not null;unique"`
+	Description string    `gorm:"type:text;not null"`
+	Link        string    `gorm:"type:text;not null"`
+	Since       time.Time `gorm:"precision:6"`
+	Until       time.Time `gorm:"precision:6"`
+	CreatedAt   time.Time `gorm:"precision:6"`
+	UpdatedAt   time.Time `gorm:"precision:6"`
+}
+
+type ContestTeam struct {
+	ID          uuid.UUID `gorm:"type:char(36);not null;primary_key"`
+	ContestID   uuid.UUID `gorm:"type:char(36);not null;unique"`
+	Description string    `gorm:"type:text;not null"`
+	Name        string    `gorm:"type:varchar(32);not null;unique"`
+	result      string    `gorm:"type:text;not null"`
+	Link        string    `gorm:"type:text;not null"`
+	CreatedAt   time.Time `gorm:"precision:6"`
+	UpdatedAt   time.Time `gorm:"precision:6"`
+}
