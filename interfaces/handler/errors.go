f422efb46aa9f8cf6055bcbb66bc99c291427767
Hosshii <sao_heath6147.wistre@icloud.com>
2021-04-16 17:54:14 +0900

    define convertError

---
+++ interfaces/handler/errors.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/traPortfolio/usecases/repository"
+)
+
+func convertError(err error) error {
+	switch {
+	case errors.Is(err, repository.ErrNilID):
+		fallthrough
+	case errors.Is(err, repository.ErrInvalidID):
+		fallthrough
+	case errors.Is(err, repository.ErrInvalidArg):
+		return echo.NewHTTPError(http.StatusBadRequest, fmt.Errorf("bad request: %w", err))
+
+	case errors.Is(err, repository.ErrAlreadyExists):
+		return echo.NewHTTPError(http.StatusConflict, fmt.Errorf("conflicts: %w", err))
+
+	case errors.Is(err, repository.ErrForbidden):
+		return echo.NewHTTPError(http.StatusForbidden, fmt.Errorf("forbideen: %w", err))
+
+	case errors.Is(err, repository.ErrNotFound):
+		return echo.NewHTTPError(http.StatusNotFound, fmt.Errorf("not found: %w", err))
+
+	default:
+		return echo.NewHTTPError(http.StatusInternalServerError, fmt.Errorf("%w", err))
+	}
+}
