87f90b74def49bc67e3d831c697a61cbb50a766e
Hosshii <sao_heath6147.wistre@icloud.com>
2021-10-31 23:30:18 +0900

    get contest test

--- interfaces/handler/contest_test.go
+++ interfaces/handler/contest_test.go
@@ -1,16 +1,20 @@
-package handler
+package handler_test
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
 	"testing"
 	"time"
 
 	"github.com/gofrs/uuid"
 	"github.com/golang/mock/gomock"
-	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/traPtitech/traPortfolio/domain"
+	"github.com/traPtitech/traPortfolio/interfaces/handler"
+	"github.com/traPtitech/traPortfolio/usecases/repository"
+	"github.com/traPtitech/traPortfolio/util"
 )
 
 func mustParseTime(layout, value string) time.Time {
@@ -23,21 +27,20 @@ func mustParseTime(layout, value string) time.Time {
 
 func TestContestHandler_GetContests(t *testing.T) {
 	tests := []struct {
-		name         string
-		setup        func(th *TestHandlers, want []*domain.Contest) (handler echo.HandlerFunc, path string)
-		statusCode   int
-		dbContest    []*domain.Contest
-		expectedBody []*ContestResponse
-		assertion    assert.ErrorAssertionFunc
+		name          string
+		setup         func(th *handler.TestHandlers, want []*domain.Contest) (path string)
+		statusCode    int
+		repo_contests []*domain.Contest
+		hres_contests []*handler.ContestResponse
 	}{
 		{
 			name: "success",
-			setup: func(th *TestHandlers, want []*domain.Contest) (handler echo.HandlerFunc, path string) {
+			setup: func(th *handler.TestHandlers, want []*domain.Contest) string {
 				th.Repository.MockContestRepository.EXPECT().GetContests().Return(want, nil)
-				return th.API.Contest.GetContests, "/api/v1/contests"
+				return "/api/v1/contests"
 			},
-			statusCode: 200,
-			dbContest: []*domain.Contest{
+			statusCode: http.StatusOK,
+			repo_contests: []*domain.Contest{
 				{
 					ID:        uuid.Nil,
 					Name:      "test1",
@@ -45,16 +48,15 @@ func TestContestHandler_GetContests(t *testing.T) {
 					TimeEnd:   mustParseTime(time.RFC3339, "2006-01-02T15:04:05+09:00"),
 				},
 			},
-			expectedBody: []*ContestResponse{
+			hres_contests: []*handler.ContestResponse{
 				{
 					Name: "test1",
-					Duration: Duration{
+					Duration: handler.Duration{
 						Since: mustParseTime(time.RFC3339, "2006-01-02T15:04:05+09:00"),
 						Until: mustParseTime(time.RFC3339, "2006-01-02T15:04:05+09:00"),
 					},
 				},
 			},
-			assertion: assert.NoError,
 		},
 	}
 	for _, tt := range tests {
@@ -63,52 +65,143 @@ func TestContestHandler_GetContests(t *testing.T) {
 			ctrl := gomock.NewController(t)
 			handlers := SetupTestHandlers(t, ctrl)
 
-			for i, v := range tt.expectedBody {
-				tt.dbContest[i].ID = v.ID
+			for i, v := range tt.hres_contests {
+				tt.repo_contests[i].ID = v.ID
 			}
-			handler, path := tt.setup(&handlers, tt.dbContest)
+			path := tt.setup(&handlers, tt.repo_contests)
 
-			var resBody []*ContestResponse
-			statusCode, _, err := doRequest(t, handler, http.MethodGet, path, nil, &resBody)
+			var resBody []*handler.ContestResponse
+			statusCode, _ := doRequest(t, handlers.API, http.MethodGet, path, nil, &resBody)
 
 			// Assertion
-			tt.assertion(t, err)
+
 			assert.Equal(t, tt.statusCode, statusCode)
-			assert.Equal(t, tt.expectedBody, resBody)
+			assert.Equal(t, tt.hres_contests, resBody)
 		})
 	}
 }
 
-// func TestContestHandler_GetContest(t *testing.T) {
-// 	type fields struct {
-// 		srv service.ContestService
-// 	}
-// 	type args struct {
-// 		_c echo.Context
-// 	}
-// 	tests := []struct {
-// 		name      string
-// 		fields    fields
-// 		args      args
-// 		setup     func(f fields, args args)
-// 		assertion assert.ErrorAssertionFunc
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			// Setup mock
-// 			ctrl := gomock.NewController(t)
-// 			tt.fields = fields{
-// 				srv: mock_service.NewMockContestService(ctrl),
-// 			}
-// 			tt.setup(tt.fields, tt.args)
-// 			h := NewContestHandler(tt.fields.srv)
-// 			// Assertion
-// 			tt.assertion(t, h.GetContest(tt.args._c))
-// 		})
-// 	}
-// }
+var (
+	getContestID = []uuid.UUID{
+		uuid.FromStringOrNil("11111111-1111-1111-1111-111111111111"),
+		uuid.FromStringOrNil("22222222-2222-2222-2222-222222222222"),
+		uuid.FromStringOrNil("33333333-3333-3333-3333-333333333333"),
+		uuid.FromStringOrNil("44444444-4444-4444-4444-444444444444"),
+		uuid.FromStringOrNil("55555555-5555-5555-5555-555555555555"),
+	}
+)
+
+func makeContest() (*domain.ContestDetail, *handler.ContestDetailResponse) {
+	d := domain.ContestDetail{
+		Contest: domain.Contest{
+			ID:        getContestID[0],
+			Name:      util.AlphaNumeric(rand.Intn(30) + 1),
+			TimeStart: util.Time(),
+			TimeEnd:   util.Time(),
+			CreatedAt: util.Time(),
+			UpdatedAt: util.Time(),
+		},
+		Link:        util.RandURLString(),
+		Description: util.AlphaNumeric(rand.Intn(30) + 1),
+		Teams: []*domain.ContestTeam{
+			{
+				ID:        getContestID[1],
+				ContestID: getContestID[0],
+				Name:      util.AlphaNumeric(rand.Intn(30) + 1),
+				Result:    util.AlphaNumeric(rand.Intn(30) + 1),
+				CreatedAt: util.Time(),
+				UpdatedAt: util.Time(),
+			},
+			{
+				ID:        getContestID[2],
+				ContestID: getContestID[0],
+				Name:      util.AlphaNumeric(rand.Intn(30) + 1),
+				Result:    util.AlphaNumeric(rand.Intn(30) + 1),
+				CreatedAt: util.Time(),
+				UpdatedAt: util.Time(),
+			},
+		},
+	}
+
+	teams := make([]*handler.ContestTeamResponse, 0, len(d.Teams))
+	for _, v := range d.Teams {
+		teams = append(teams, &handler.ContestTeamResponse{
+			ID:     v.ID,
+			Name:   v.Name,
+			Result: v.Result,
+		})
+	}
+
+	hres := handler.ContestDetailResponse{
+		ContestResponse: handler.ContestResponse{
+			ID:   d.ID,
+			Name: d.Name,
+			Duration: handler.Duration{
+				Since: d.TimeStart,
+				Until: d.TimeEnd,
+			},
+		},
+		Link:        d.Link,
+		Description: d.Description,
+		Teams:       teams,
+	}
+
+	return &d, &hres
+}
+
+func TestContestHandler_GetContest(t *testing.T) {
+	tests := []struct {
+		name       string
+		setup      func(th *handler.TestHandlers) (repoContest *domain.ContestDetail, hresContest *handler.ContestDetailResponse, path string)
+		statusCode int
+	}{
+		{
+			name: "Success",
+			setup: func(th *handler.TestHandlers) (*domain.ContestDetail, *handler.ContestDetailResponse, string) {
+				want, hres := makeContest()
+				th.Repository.MockContestRepository.EXPECT().GetContest(want.ID).Return(want, nil)
+				path := fmt.Sprintf("/api/v1/contests/%s", want.ID.String())
+
+				return want, hres, path
+			},
+			statusCode: http.StatusOK,
+		},
+		{
+			name: "Invalid ID",
+			setup: func(th *handler.TestHandlers) (*domain.ContestDetail, *handler.ContestDetailResponse, string) {
+				path := "/api/v1/contests/invalid"
+				return &domain.ContestDetail{}, &handler.ContestDetailResponse{}, path
+			},
+			statusCode: http.StatusBadRequest,
+		},
+		{
+			name: "Not Found",
+			setup: func(th *handler.TestHandlers) (*domain.ContestDetail, *handler.ContestDetailResponse, string) {
+				uid := util.UUID()
+				th.Repository.MockContestRepository.EXPECT().GetContest(uid).Return(nil, repository.ErrNotFound)
+
+				return &domain.ContestDetail{}, &handler.ContestDetailResponse{}, fmt.Sprintf("/api/v1/contests/%s", uid)
+			},
+			statusCode: http.StatusNotFound,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Setup mock
+			ctrl := gomock.NewController(t)
+			handlers := SetupTestHandlers(t, ctrl)
+
+			_, hresContest, path := tt.setup(&handlers)
+
+			var resBody handler.ContestDetailResponse
+			statusCode, _ := doRequest(t, handlers.API, http.MethodGet, path, nil, &resBody)
+
+			// Assertion
+			assert.Equal(t, tt.statusCode, statusCode)
+			assert.Equal(t, hresContest, &resBody)
+		})
+	}
+}
 
 // func TestContestHandler_PostContest(t *testing.T) {
 // 	type fields struct {
