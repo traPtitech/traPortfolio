429ecf19983a3e53fd5502f0c800d7f423ad2017
Ras96 <asymptote.k.k@gmail.com>
2021-12-16 21:37:08 +0900

    :white_check_mark: Add GetProjects TestCase

--- interfaces/repository/project_impl_test.go
+++ interfaces/repository/project_impl_test.go
@@ -1,8 +1,12 @@
 package repository
 
 import (
+	"math/rand"
+	"regexp"
 	"testing"
+	"time"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/gofrs/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/traPtitech/traPortfolio/domain"
@@ -10,6 +14,7 @@ import (
 	"github.com/traPtitech/traPortfolio/interfaces/database/mock_database"
 	"github.com/traPtitech/traPortfolio/interfaces/repository/model"
 	"github.com/traPtitech/traPortfolio/usecases/repository"
+	"github.com/traPtitech/traPortfolio/util/random"
 )
 
 type mockProjectRepositoryFields struct {
@@ -30,7 +35,42 @@ func TestProjectRepository_GetProjects(t *testing.T) {
 		setup     func(f mockProjectRepositoryFields, want []*domain.Project)
 		assertion assert.ErrorAssertionFunc
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Success",
+			want: []*domain.Project{
+				{
+					ID:          random.UUID(),
+					Name:        random.AlphaNumeric(rand.Intn(30) + 1),
+					Since:       time.Now(),
+					Until:       time.Now(),
+					Description: random.AlphaNumeric(rand.Intn(30) + 1),
+					Link:        random.RandURLString(),
+					Members:     nil,
+				},
+			},
+			setup: func(f mockProjectRepositoryFields, want []*domain.Project) {
+				rows := sqlmock.NewRows([]string{"id", "name", "description", "link", "since", "until"})
+				for _, v := range want {
+					rows.AddRow(v.ID, v.Name, v.Description, v.Link, v.Since, v.Until)
+				}
+				h := f.h.(*mock_database.MockSQLHandler)
+				h.Mock.
+					ExpectQuery(regexp.QuoteMeta("SELECT * FROM `projects`")).
+					WillReturnRows(rows)
+			},
+			assertion: assert.NoError,
+		},
+		{
+			name: "UnexpectedError",
+			want: nil,
+			setup: func(f mockProjectRepositoryFields, want []*domain.Project) {
+				h := f.h.(*mock_database.MockSQLHandler)
+				h.Mock.
+					ExpectQuery(regexp.QuoteMeta("SELECT * FROM `projects`")).
+					WillReturnError(errUnexpected)
+			},
+			assertion: assert.Error,
+		},
 	}
 	for _, tt := range tests {
 		tt := tt
