bdfd09fc03374aebd70d412ea6382744b7d92c14
Hosshii <sao_heath6147.wistre@icloud.com>
2022-01-06 23:10:48 +0900

    test get by id

--- interfaces/handler/project_test.go
+++ interfaces/handler/project_test.go
@@ -2,6 +2,7 @@ package handler_test
 
 import (
 	"errors"
+	"fmt"
 	"math/rand"
 	"net/http"
 	"testing"
@@ -84,7 +85,7 @@ func TestProjecttHandler_GetAll(t *testing.T) {
 				var reqBody []*handler.Project
 				for _, v := range repo {
 					reqBody = append(reqBody, &handler.Project{
-						Duration: handler.ProjectDuration{
+						Duration: handler.YearWithSemesterDuration{
 							Since: handler.YearWithSemester{
 								Semester: handler.Semester(sinceSem),
 								Year:     v.Since.Year(),
@@ -122,6 +123,113 @@ func TestProjecttHandler_GetAll(t *testing.T) {
 			expectedHres, path := tt.setup(&handlers)
 
 			hres := []*handler.Project(nil)
+			statusCode, _ := doRequest(t, handlers.API, http.MethodGet, path, nil, &hres)
+
+			// Assertion
+			assert.Equal(t, tt.statusCode, statusCode)
+			assert.Equal(t, expectedHres, hres)
+		})
+	}
+}
+
+func TestProjecttHandler_GetByID(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name       string
+		setup      func(th *handler.TestHandlers) (handler.ProjectDetail, string)
+		statusCode int
+	}{
+		{
+			name: "Success",
+			setup: func(th *handler.TestHandlers) (handler.ProjectDetail, string) {
+				sinceSem := rand.Intn(2)
+				untilSem := rand.Intn(2)
+				projectID := random.UUID()
+				repo := domain.Project{
+					ID:          projectID,
+					Name:        random.AlphaNumeric(rand.Intn(30) + 1),
+					Since:       makeSemesterTime(sinceSem),
+					Until:       makeSemesterTime(untilSem),
+					Description: random.AlphaNumeric(rand.Intn(30) + 1),
+					Link:        random.RandURLString(),
+					Members: []*domain.ProjectMember{
+						{
+							UserID:   random.UUID(),
+							Name:     random.AlphaNumeric(rand.Intn(30) + 1),
+							RealName: random.AlphaNumeric(rand.Intn(30) + 1),
+							Since:    random.Time(),
+							Until:    random.Time(),
+						},
+					},
+				}
+
+				var members []handler.ProjectMember
+				for _, v := range repo.Members {
+					members = append(members, handler.ProjectMember{
+						User: handler.User{
+							Id:       v.UserID,
+							Name:     v.Name,
+							RealName: v.RealName,
+						},
+						Duration: []handler.YearWithSemesterDuration{
+							{
+								Since: handler.YearWithSemester{
+									Semester: handler.Semester(sinceSem),
+									Year:     v.Since.Year(),
+								},
+								Until: &handler.YearWithSemester{
+									Semester: handler.Semester(untilSem),
+									Year:     v.Until.Year(),
+								},
+							},
+						},
+					})
+				}
+				reqBody := handler.ProjectDetail{
+					Project: handler.Project{
+						Duration: handler.YearWithSemesterDuration{
+							Since: handler.YearWithSemester{
+								Semester: handler.Semester(sinceSem),
+								Year:     repo.Since.Year(),
+							},
+							Until: &handler.YearWithSemester{
+								Semester: handler.Semester(untilSem),
+								Year:     repo.Until.Year(),
+							},
+						},
+						Id:   repo.ID,
+						Name: repo.Name,
+					},
+					Description: repo.Description,
+					Link:        repo.Link,
+					Members:     members,
+				}
+
+				th.Service.MockProjectService.EXPECT().GetProject(gomock.Any(), projectID).Return(&repo, nil)
+				return reqBody, fmt.Sprintf("/api/v1/projects/%s", projectID)
+			},
+			statusCode: http.StatusOK,
+		},
+		{
+			name: "Internal Error",
+			setup: func(th *handler.TestHandlers) (handler.ProjectDetail, string) {
+				projectID := random.UUID()
+				th.Service.MockProjectService.EXPECT().GetProject(gomock.Any(), projectID).Return(nil, errInternal)
+				return handler.ProjectDetail{}, fmt.Sprintf("/api/v1/projects/%s", projectID)
+			},
+			statusCode: http.StatusInternalServerError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Setup mock
+			ctrl := gomock.NewController(t)
+			handlers := SetupTestHandlers(t, ctrl)
+
+			expectedHres, path := tt.setup(&handlers)
+
+			hres := handler.ProjectDetail{}
 			statusCode, _ := doRequest(t, handlers.API, http.MethodGet, path, nil, &hres)
 
 			// Assertion
