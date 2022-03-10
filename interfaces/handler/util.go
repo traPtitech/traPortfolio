24a5d6425869749a9e81ce55c91b52051e29a326
Ras96 <asymptote.k.k@gmail.com>
2022-03-11 00:28:08 +0900

    :sparkles: Add oapi_client_gen.go

--- interfaces/handler/util.go
+++ interfaces/handler/util.go
@@ -1,4 +1,5 @@
 //go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest --config ../../.oapi.types.yml ../../docs/swagger/traPortfolio.v1.yaml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest --config ../../.oapi.client.yml ../../docs/swagger/traPortfolio.v1.yaml
 
 package handler
 
