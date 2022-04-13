a34ec1dbf511ffc1f4066498aac715ce9002793f
Ras96 <asymptote.k.k@gmail.com>
2022-04-13 20:22:25 +0900

    :bug: GetUserのフィールド抜け修正 & テスト
    data raceが発生してる

--- integration_tests/handler/user_test.go
+++ integration_tests/handler/user_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"github.com/traPtitech/traPortfolio/integration_tests/testutils"
@@ -30,6 +31,21 @@ var (
 		Name:     mockdata.MockUsers[2].Name,
 		RealName: mockdata.MockPortalUsers[2].RealName,
 	}
+
+	sampleAccount = handler.Account{
+		DisplayName: mockdata.MockAccount.Name,
+		Id:          mockdata.MockAccount.ID,
+		PrPermitted: handler.PrPermitted(mockdata.MockAccount.Check),
+		Type:        handler.AccountType(mockdata.MockAccount.Type),
+		Url:         mockdata.MockAccount.URL,
+	}
+
+	sampleUserDetail1 = handler.UserDetail{
+		User:     sampleUser1,
+		Accounts: []handler.Account{sampleAccount},
+		Bio:      mockdata.MockUsers[0].Description,
+		State:    handler.UserAccountState(mockdata.MockTraQUsers[0].User.State),
+	}
 )
 
 // GetUsers GET /users
@@ -43,7 +59,7 @@ func TestGetUsers(t *testing.T) {
 	tests := map[string]struct {
 		statusCode int
 		params     handler.GetUsersParams
-		want       interface{}
+		want       interface{} // []handler.User | echo.HTTPError
 	}{
 		"200": {
 			http.StatusOK,
@@ -92,6 +108,34 @@ func TestGetUsers(t *testing.T) {
 		tt := tt
 		t.Run(name, func(t *testing.T) {
 			res := testutils.DoRequest(t, e, http.MethodGet, e.URL(api.User.GetUsers), &tt.params)
+			testutils.AssertResponse(t, tt.statusCode, tt.want, res)
+		})
+	}
+}
+
+// GET /users/:userID
+func TestGetUser(t *testing.T) {
+	t.Parallel()
+	tests := map[string]struct {
+		statusCode int
+		userID     uuid.UUID
+		want       interface{} // handler.UserDetail | echo.HTTPError
+	}{
+		"200": {
+			http.StatusOK,
+			sampleUserDetail1.Id,
+			sampleUserDetail1,
+		},
+	}
+
+	e := echo.New()
+	conf := testutils.GetConfigWithDBName("get_user")
+	api, err := testutils.SetupRoutes(t, e, conf)
+	assert.NoError(t, err)
+	for name, tt := range tests {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			res := testutils.DoRequest(t, e, http.MethodGet, e.URL(api.User.GetByID, tt.userID.String()), nil)
 			testutils.AssertResponse(t, tt.statusCode, tt.want, res)
 		})
 	}
