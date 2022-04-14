0f68c38bb603810a362511c345a55835a814936a
Hosshii <sao_heath6147.wistre@icloud.com>
2022-04-14 21:58:21 +0900

    util

--- util/config/config_test.go
+++ util/config/config_test.go
@@ -1,41 +1,40 @@
-package config_test
+package config
 
 import (
 	"testing"
 
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
-	"github.com/traPtitech/traPortfolio/util/config"
 )
 
 func TestParse(t *testing.T) {
 	viper.AddConfigPath("./testdata")
-	config.Parse()
+	Parse()
 
-	expected := config.Config{
+	expected := Config{
 		IsProduction: true,
 		Port:         3000,
 		Migrate:      true,
-		DB: config.SQLConfig{
+		DB: SQLConfig{
 			User: "root",
 			Pass: "password",
 			Host: "mysql",
 			Name: "portfolio",
 			Port: 3001,
 		},
-		Traq: config.TraqConfig{
+		Traq: TraqConfig{
 			Cookie:      "traq cookie",
 			APIEndpoint: "traq endpoint",
 		},
-		Knoq: config.KnoqConfig{
+		Knoq: KnoqConfig{
 			Cookie:      "knoq cookie",
 			APIEndpoint: "knoq endpoint",
 		},
-		Portal: config.PortalConfig{
+		Portal: PortalConfig{
 			Cookie:      "portal cookie",
 			APIEndpoint: "portal endpoint",
 		},
 	}
 
-	assert.Equal(t, &expected, config.GetConfig())
+	assert.Equal(t, &expected, GetConfig())
 }
