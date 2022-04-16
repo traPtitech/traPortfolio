5aa850c4a9aaf2ba90f4d33cf6bddc43eedd5f43
Hosshii <sao_heath6147.wistre@icloud.com>
2022-04-16 15:29:06 +0900

    fix db name

--- integration_tests/testutils/db.go
+++ integration_tests/testutils/db.go
@@ -100,7 +100,7 @@ func dropAll(t *testing.T, db *gorm.DB) {
 }
 
 func testDBName(dbName string) string {
-	const dbPrefix = "portfolio_test_repo_"
+	const dbPrefix = "portfolio_test_"
 
 	return dbPrefix + dbName
 }
