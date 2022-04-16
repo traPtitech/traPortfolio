c6b2a999621e96bcc412822e23259ec8907ef7c7
Ras96 <asymptote.k.k@gmail.com>
2022-04-16 21:58:51 +0900

    :adhesive_bandage: Fix sql

--- interfaces/repository/user_impl.go
+++ interfaces/repository/user_impl.go
@@ -1,8 +1,6 @@
 package repository
 
 import (
-	"log"
-
 	"github.com/gofrs/uuid"
 	"github.com/traPtitech/traPortfolio/domain"
 	"github.com/traPtitech/traPortfolio/interfaces/database"
@@ -151,7 +149,7 @@ func (repo *UserRepository) GetUser(userID uuid.UUID) (*domain.UserDetail, error
 func (repo *UserRepository) CreateUser(args repository.CreateUserArgs) (*domain.UserDetail, error) {
 	portalUser, err := repo.portal.GetByTraqID(args.Name)
 	if err != nil {
-		log.Println(err)
+		return nil, convertError(err)
 	}
 
 	user := model.User{
