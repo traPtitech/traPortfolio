30682939adbdcd454b538058c5453f4b58233050
Ras96 <asymptote.k.k@gmail.com>
2022-05-19 10:24:57 +0900

    :recycle: Separate var and func

--- util/mockdata/model.go
+++ util/mockdata/model.go
@@ -9,132 +9,133 @@ import (
 )
 
 var (
-	MockUsers      = CloneMockUsers()
-	CloneMockUsers = func() []*model.User {
-		return []*model.User{
-			{
-				ID:          userID1,
-				Description: "I am user1",
-				Check:       true,
-				Name:        userName1,
-			},
-			{
-				ID:          userID2,
-				Description: "I am user2",
-				Check:       true,
-				Name:        userName2,
-			},
-			{
-				ID:          userID3,
-				Description: "I am lolico",
-				Check:       false,
-				Name:        userName3,
-			},
-		}
-	}
-
-	MockAccount      = CloneMockAccount()
-	CloneMockAccount = func() model.Account {
-		return model.Account{
-			ID:     accountID,
-			Type:   0,
-			Name:   "sample_account_display_name",
-			URL:    "https://sample.accounts.com",
-			UserID: userID1,
-			Check:  true,
-		}
-	}
-
-	MockContest      = CloneMockContest()
-	CloneMockContest = func() model.Contest {
-		return model.Contest{
-			ID:          contestID,
-			Name:        "sample_contest_name",
-			Description: "sample_contest_description",
-			Link:        "https://sample.contests.com",
-			Since:       time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC),
-			Until:       time.Date(2022, 1, 2, 0, 0, 0, 0, time.UTC),
-		}
-	}
-
-	MockContestTeam      = CloneMockContestTeam()
-	CloneMockContestTeam = func() model.ContestTeam {
-		return model.ContestTeam{
-			ID:          contestTeamID,
-			ContestID:   contestID,
-			Name:        "sample_contest_team_name",
-			Description: "sample_contest_team_description",
-			Result:      "sample_contest_team_result",
-			Link:        "https://sample.contest_teams.com",
-		}
-	}
-
-	MockContestTeamUserBelonging      = CloneMockContestTeam()
-	CloneMockContestTeamUserBelonging = func() model.ContestTeamUserBelonging {
-		return model.ContestTeamUserBelonging{
-			TeamID: contestTeamID,
-			UserID: userID1,
-		}
-	}
-
-	MockEventLevelRelation      = CloneMockEventLevelRelation()
-	CloneMockEventLevelRelation = func() model.EventLevelRelation {
-		return model.EventLevelRelation{
-			ID:    eventID,
-			Level: domain.EventLevelPublic,
-		}
-	}
-
-	MockGroup      = CloneMockGroup()
-	CloneMockGroup = func() model.Group {
-		return model.Group{
-			GroupID:     groupID,
-			Name:        "sample_group_name",
-			Link:        "https://sample.groups.com",
-			Leader:      userID1,
-			Description: "sample_group_description",
-		}
-	}
-
-	MockGroupUserBelonging      = CloneMockGroupUserBelonging()
-	CloneMockGroupUserBelonging = func() model.GroupUserBelonging {
-		return model.GroupUserBelonging{
-			UserID:        userID1,
-			GroupID:       MockGroup.GroupID,
-			SinceYear:     2022,
-			SinceSemester: 1,
-			UntilYear:     2022,
-			UntilSemester: 2,
-		}
-	}
-
-	MockProject      = CloneMockProject()
-	CloneMockProject = func() model.Project {
-		return model.Project{
-			ID:            projectID,
-			Name:          "sample_project_name",
-			Description:   "sample_project_description",
-			Link:          "https://sample.projects.com",
-			SinceYear:     2022,
-			SinceSemester: 1,
-			UntilYear:     2022,
-			UntilSemester: 2,
-		}
-	}
-
-	MockProjectMember      = CloneMockProjectMember()
-	CloneMockProjectMember = func() model.ProjectMember {
-		return model.ProjectMember{
-			ID:            projectMemberID,
-			ProjectID:     projectID,
-			UserID:        userID1,
-			SinceYear:     2022,
-			SinceSemester: 1,
-			UntilYear:     2022,
-			UntilSemester: 2,
-		}
-	}
+	MockUsers                    = CloneMockUsers()
+	MockAccount                  = CloneMockAccount()
+	MockContest                  = CloneMockContest()
+	MockContestTeam              = CloneMockContestTeam()
+	MockContestTeamUserBelonging = CloneMockContestTeam()
+	MockEventLevelRelation       = CloneMockEventLevelRelation()
+	MockGroup                    = CloneMockGroup()
+	MockGroupUserBelonging       = CloneMockGroupUserBelonging()
+	MockProject                  = CloneMockProject()
+	MockProjectMember            = CloneMockProjectMember()
 )
+
+func CloneMockUsers() []*model.User {
+	return []*model.User{
+		{
+			ID:          userID1,
+			Description: "I am user1",
+			Check:       true,
+			Name:        userName1,
+		},
+		{
+			ID:          userID2,
+			Description: "I am user2",
+			Check:       true,
+			Name:        userName2,
+		},
+		{
+			ID:          userID3,
+			Description: "I am lolico",
+			Check:       false,
+			Name:        userName3,
+		},
+	}
+}
+
+func CloneMockAccount() model.Account {
+	return model.Account{
+		ID:     accountID,
+		Type:   0,
+		Name:   "sample_account_display_name",
+		URL:    "https://sample.accounts.com",
+		UserID: userID1,
+		Check:  true,
+	}
+}
+
+func CloneMockContest() model.Contest {
+	return model.Contest{
+		ID:          contestID,
+		Name:        "sample_contest_name",
+		Description: "sample_contest_description",
+		Link:        "https://sample.contests.com",
+		Since:       time.Date(2022, 1, 1, 0, 0, 0, 0, time.UTC),
+		Until:       time.Date(2022, 1, 2, 0, 0, 0, 0, time.UTC),
+	}
+}
+
+func CloneMockContestTeam() model.ContestTeam {
+	return model.ContestTeam{
+		ID:          contestTeamID,
+		ContestID:   contestID,
+		Name:        "sample_contest_team_name",
+		Description: "sample_contest_team_description",
+		Result:      "sample_contest_team_result",
+		Link:        "https://sample.contest_teams.com",
+	}
+}
+
+func CloneMockContestTeamUserBelonging() model.ContestTeamUserBelonging {
+	return model.ContestTeamUserBelonging{
+		TeamID: contestTeamID,
+		UserID: userID1,
+	}
+}
+
+func CloneMockEventLevelRelation() model.EventLevelRelation {
+	return model.EventLevelRelation{
+		ID:    eventID,
+		Level: domain.EventLevelPublic,
+	}
+}
+
+func CloneMockGroup() model.Group {
+	return model.Group{
+		GroupID:     groupID,
+		Name:        "sample_group_name",
+		Link:        "https://sample.groups.com",
+		Leader:      userID1,
+		Description: "sample_group_description",
+	}
+}
+
+func CloneMockGroupUserBelonging() model.GroupUserBelonging {
+	return model.GroupUserBelonging{
+		UserID:        userID1,
+		GroupID:       MockGroup.GroupID,
+		SinceYear:     2022,
+		SinceSemester: 1,
+		UntilYear:     2022,
+		UntilSemester: 2,
+	}
+}
+
+func CloneMockProject() model.Project {
+	return model.Project{
+		ID:            projectID,
+		Name:          "sample_project_name",
+		Description:   "sample_project_description",
+		Link:          "https://sample.projects.com",
+		SinceYear:     2022,
+		SinceSemester: 1,
+		UntilYear:     2022,
+		UntilSemester: 2,
+	}
+}
+
+func CloneMockProjectMember() model.ProjectMember {
+	return model.ProjectMember{
+		ID:            projectMemberID,
+		ProjectID:     projectID,
+		UserID:        userID1,
+		SinceYear:     2022,
+		SinceSemester: 1,
+		UntilYear:     2022,
+		UntilSemester: 2,
+	}
+}
 
 func InsertSampleDataToDB(h database.SQLHandler) error {
 	mockUsers := CloneMockUsers()
