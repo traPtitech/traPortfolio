8abe340b00ba54a90cd89e6cca84c035da68d698
Ras96 <asymptote.k.k@gmail.com>
2022-05-27 14:34:03 +0900

    :white_check_mark: Test GetUserContests

--- integration_tests/handler/user_test.go
+++ integration_tests/handler/user_test.go
@@ -612,30 +612,49 @@ func TestGetUserProjects(t *testing.T) {
 	}
 }
 
-// // GetUserContests GET /users/:userID/contests
-// func TestGetUserContests(t *testing.T) {
-// 	t.Parallel()
-// 	tests := map[string]struct {
-// 		statusCode int
-// 		userID     uuid.UUID
-// 		want       interface{}
-// 	}{
-// 		// TODO: Add cases
-// 	}
+// GetUserContests GET /users/:userID/contests
+func TestGetUserContests(t *testing.T) {
+	t.Parallel()
+	tests := map[string]struct {
+		statusCode int
+		userID     uuid.UUID
+		want       interface{}
+	}{
+		"200": {
+			http.StatusOK,
+			mockdata.HMockUsers[0].Id,
+			mockdata.HMockUserContests,
+		},
+		"200 no contests with existing userID": {
+			http.StatusOK,
+			mockdata.HMockUsers[1].Id,
+			[]handler.Contest{},
+		},
+		"200 no contests with non-existing userID": {
+			http.StatusOK,
+			random.UUID(),
+			[]handler.Contest{},
+		},
+		"400 invalid userID": {
+			http.StatusBadRequest,
+			uuid.Nil,
+			handler.ConvertError(t, repository.ErrValidate),
+		},
+	}
 
-// 	e := echo.New()
-// 	conf := testutils.GetConfigWithDBName("user_handler_get_user_contests")
-// 	api, err := testutils.SetupRoutes(t, e, conf)
-// 	assert.NoError(t, err)
-// 	for name, tt := range tests {
-// 		tt := tt
-// 		t.Run(name, func(t *testing.T) {
-//    	t.Parallel()
-// 			res := testutils.DoRequest(t, e, http.MethodGet, e.URL(api.User.GetUserContests, tt.userID), nil)
-// 			testutils.AssertResponse(t, tt.statusCode, tt.want, res)
-// 		})
-// 	}
-// }
+	e := echo.New()
+	conf := testutils.GetConfigWithDBName("user_handler_get_user_contests")
+	api, err := testutils.SetupRoutes(t, e, conf)
+	assert.NoError(t, err)
+	for name, tt := range tests {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			res := testutils.DoRequest(t, e, http.MethodGet, e.URL(api.User.GetUserContests, tt.userID), nil)
+			testutils.AssertResponse(t, tt.statusCode, tt.want, res)
+		})
+	}
+}
 
 // // GetUserGroups GET /users/:userID/groups
 // func TestGetUserGroups(t *testing.T) {
