6c3f68758b6147c52e02ff4f2df5cc5a4c14cdb5
grkon <yo.for.user@gmail.com>
2022-07-06 22:51:33 +0900

    TestGetGroupsを作成

---
+++ integration_tests/handler/group_test.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/traPtitech/traPortfolio/integration_tests/testutils"
+	"github.com/traPtitech/traPortfolio/interfaces/handler"
+	"github.com/traPtitech/traPortfolio/util/mockdata"
+)
+
+// GetGroups GET /groups
+func TestGetGroups(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		statusCode int
+		want       interface{}
+	}{
+		"200": {
+			http.StatusOK,
+			[]handler.Group{
+				{
+					Id:   mockdata.HMockGroup.Id,
+					Name: mockdata.HMockGroup.Name,
+				},
+			},
+		},
+	}
+
+	e := echo.New()
+	conf := testutils.GetConfigWithDBName("group_handler_get_groups")
+	api, err := testutils.SetupRoutes(t, e, conf)
+
+	assert.NoError(t, err)
+
+	for name, tt := range tests {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			res := testutils.DoRequest(t, e, http.MethodGet, e.URL(api.Group.GetGroups), nil)
+			testutils.AssertResponse(t, tt.statusCode, tt.want, res)
+		})
+	}
+}
