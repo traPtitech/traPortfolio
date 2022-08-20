d930ec2d5d13b71a5e3bd50955ed05851a25037c
mehm8128 <mehm8128@gmail.com>
2022-08-20 19:22:19 +0900

    add duplicate check

--- interfaces/repository/project_impl.go
+++ interfaces/repository/project_impl.go
@@ -203,6 +203,15 @@ func (repo *ProjectRepository) AddProjectMembers(projectID uuid.UUID, projectMem
 		return repository.ErrInvalidArg
 	}
 
+	// ユーザーの重複チェック
+	projectMembersMap := make(map[uuid.UUID]struct{}, len(projectMembers))
+	for _, v := range projectMembers {
+		if _, exist := projectMembersMap[v.UserID]; exist {
+			return repository.ErrInvalidArg
+		}
+		projectMembersMap[v.UserID] = struct{}{}
+	}
+
 	// プロジェクトの存在チェック
 	err := repo.h.
 		Where(&model.Project{ID: projectID}).
