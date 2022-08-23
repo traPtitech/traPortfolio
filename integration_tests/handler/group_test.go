9713a53ef8c5bdaec371a37da09f7fb991926cca
grkon <yo.for.user@gmail.com>
2022-08-23 22:21:25 +0900

    TestGetGroup を作成

--- integration_tests/handler/group_test.go
+++ integration_tests/handler/group_test.go
@@ -4,10 +4,12 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/gofrs/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"github.com/traPtitech/traPortfolio/integration_tests/testutils"
 	"github.com/traPtitech/traPortfolio/interfaces/handler"
+	"github.com/traPtitech/traPortfolio/usecases/repository"
 	"github.com/traPtitech/traPortfolio/util/mockdata"
 )
 
@@ -41,6 +43,48 @@ func TestGetGroups(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 			res := testutils.DoRequest(t, e, http.MethodGet, e.URL(api.Group.GetGroups), nil)
+			testutils.AssertResponse(t, tt.statusCode, tt.want, res)
+		})
+	}
+}
+
+// GetGroup GET /groups/:groupID
+func TestGetGroup(t *testing.T) {
+	t.Parallel()
+
+	tests := map[string]struct {
+		statusCode int
+		groupID    uuid.UUID
+		want       interface{}
+	}{
+		"200": {
+			statusCode: http.StatusOK,
+			groupID:    mockdata.HMockGroup.Id,
+			want:       mockdata.HMockGroup,
+		},
+		"400 invalid userID": {
+			statusCode: http.StatusBadRequest,
+			groupID:    uuid.Nil,
+			want:       handler.ConvertError(t, repository.ErrValidate),
+		},
+		"404": {
+			statusCode: http.StatusNotFound,
+			groupID:    uuid.Nil,
+			want:       handler.ConvertError(t, repository.ErrNotFound),
+		},
+	}
+
+	e := echo.New()
+	conf := testutils.GetConfigWithDBName("group_handler_get_group")
+	api, err := testutils.SetupRoutes(t, e, conf)
+
+	assert.NoError(t, err)
+
+	for name, tt := range tests {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			res := testutils.DoRequest(t, e, http.MethodGet, e.URL(api.Group.GetGroup), tt.groupID)
 			testutils.AssertResponse(t, tt.statusCode, tt.want, res)
 		})
 	}
