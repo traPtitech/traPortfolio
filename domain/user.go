2cb25c7a340b56287bd82093878427eb151d2d2c
ras0q <ras0q@users.noreply.github.com>
2024-06-18 18:08:59 +0900

    :sparkles: allow x.com (and fix validation)

--- domain/user.go
+++ domain/user.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"fmt"
+	"net/url"
 	"regexp"
 	"time"
 
@@ -121,19 +122,23 @@ func (a AccountType) Value() (driver.Value, error) {
 }
 
 func IsValidAccountURL(accountType AccountType, URL string) bool {
+	if _, err := url.Parse(URL); err != nil {
+		return false
+	}
+
 	var urlRegexp = map[AccountType]*regexp.Regexp{
 		HOMEPAGE:   regexp.MustCompile("^https?://.+$"),
 		BLOG:       regexp.MustCompile("^https?://.+$"),
-		TWITTER:    regexp.MustCompile("^https://twitter.com/[a-zA-Z0-9_]+$"),
-		FACEBOOK:   regexp.MustCompile("^https://www.facebook.com/[a-zA-Z0-9.]+$"),
-		PIXIV:      regexp.MustCompile("^https://www.pixiv.net/users/[0-9]+"),
-		GITHUB:     regexp.MustCompile("^https://github.com/[a-zA-Z0-9-]+$"),
-		QIITA:      regexp.MustCompile("^https://qiita.com/[a-zA-Z0-9-_]+$"),
-		ZENN:       regexp.MustCompile("^https://zenn.dev/[a-zA-Z0-9.]+$"),
-		ATCODER:    regexp.MustCompile("^https://atcoder.jp/users/[a-zA-Z0-9_]+$"),
-		SOUNDCLOUD: regexp.MustCompile("^https://soundcloud.com/[a-z0-9-_]+$"),
-		HACKTHEBOX: regexp.MustCompile("^https://app.hackthebox.com/users/[a-zA-Z0-9]+$"),
-		CTFTIME:    regexp.MustCompile("^https://ctftime.org/user/[0-9]+$"),
+		TWITTER:    regexp.MustCompile("^https://(twitter|x)\\.com/[a-zA-Z0-9_]+$"),
+		FACEBOOK:   regexp.MustCompile("^https://www.facebook\\.com/[a-zA-Z0-9.]+$"),
+		PIXIV:      regexp.MustCompile("^https://www.pixiv\\.net/users/[0-9]+"),
+		GITHUB:     regexp.MustCompile("^https://github\\.com/[a-zA-Z0-9-]+$"),
+		QIITA:      regexp.MustCompile("^https://qiita\\.com/[a-zA-Z0-9-_]+$"),
+		ZENN:       regexp.MustCompile("^https://zenn\\.dev/[a-zA-Z0-9.]+$"),
+		ATCODER:    regexp.MustCompile("^https://atcoder\\.jp/users/[a-zA-Z0-9_]+$"),
+		SOUNDCLOUD: regexp.MustCompile("^https://soundcloud\\.com/[a-z0-9-_]+$"),
+		HACKTHEBOX: regexp.MustCompile("^https://app.hackthebox\\.com/users/[a-zA-Z0-9]+$"),
+		CTFTIME:    regexp.MustCompile("^https://ctftime\\.org/user/[0-9]+$"),
 	}
 
 	if r, ok := urlRegexp[accountType]; ok {
